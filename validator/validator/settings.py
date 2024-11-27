9ecf729d8299a66d4dc7f1e7f0e72ca4033d9347
Tomixbo <tomixbo@gmail.com>
2024-11-27 12:18:43 +0300

    update CSRF_TRUSTED_ORIGINS

--- validator/validator/settings.py
+++ validator/validator/settings.py
@@ -28,7 +28,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
     'http://localhost',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1',
 ]
 
 
